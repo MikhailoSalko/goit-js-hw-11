c7407bef062ed957bd71dca16dcb3ce59546e938
Mikhailo Salko <slipknotmisha@gmail.com>
2023-04-02 15:28:15 +0300

    markup for gallery and class for fetchPhotos

--- src/index.js
+++ src/index.js
@@ -1,24 +1,44 @@
-import axios from 'axios';
+// import axios from 'axios';
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import simpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { fetchPhotos } from './js/fetchPhotos';
+import { Photo } from './js/fetchPhotos';
+import { renderPhotos } from './js/renderMarkup';
 
 const form = document.querySelector('.search-form');
 const loadMoreBtn = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
 
 form.addEventListener('submit', handleSubmitFetchPhotos);
-loadMoreBtn.addEventListener('click', handleLoadMoreBtnFetchPhotos);
+loadMoreBtn.addEventListener('click', fetchAndRenderMarkupOfGallery);
+
+const fetchPhoto = new Photo();
 
 async function handleSubmitFetchPhotos(e) {
   e.preventDefault();
-  console.log(e.target);
-  const photos = await fetchPhotos();
-  console.log(photos);
+
+  fetchPhoto.searchQuery = e.currentTarget.searchQuery.value.trim();
+  if (fetchPhoto.searchQuery === '') {
+    return;
+  }
+  try {
+    gallery.innerHTML = '';
+    fetchPhoto.page = 1;
+    fetchAndRenderMarkupOfGallery();
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-async function handleLoadMoreBtnFetchPhotos(e) {
-  console.log(e.target.type);
-  fetchPhotos();
+async function fetchAndRenderMarkupOfGallery() {
+  const result = await fetchPhoto.fetchPhotos();
+  const arrayOfPhotos = result.data.hits;
+  getGallery(arrayOfPhotos);
+  fetchPhoto.pageCount();
+}
+
+function getGallery(arr) {
+  gallery.insertAdjacentHTML('beforeend', renderPhotos(arr));
+  let lightbox = new SimpleLightbox('.gallery a');
 }
--- src/js/fetchPhotos.js
+++ src/js/fetchPhotos.js
@@ -1,10 +1,33 @@
 import axios from 'axios';
 
-async function fetchPhotos() {
-  const responce = await axios.get('https://pixabay.com/api/docs/');
-  const result = await responce.json();
-  console.log(result);
-  return result;
+class Photo {
+  #BASE_URL = 'https://pixabay.com/api/';
+  #API_KEY = '34983151-308c1e13d6f3ee051936793b8';
+
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchPhotos() {
+    const response = await axios.get(`${this.#BASE_URL}`, {
+      params: {
+        key: this.#API_KEY,
+        q: this.searchQuery,
+        page: this.page,
+        per_page: 40,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    });
+
+    return response;
+  }
+
+  pageCount() {
+    this.page += 1;
+  }
 }
 
-export { fetchPhotos };
+export { Photo };
